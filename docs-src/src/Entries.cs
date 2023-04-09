87220a9018372a6e9c3ade7acbf0e430d1b595fd
AlekseyMartynov <AlekseyMartynov@users.noreply.github.com>
2023-04-09 13:23:38 +0300

    docs engine: tolerate empty content

--- docs-src/src/Entries.cs
+++ docs-src/src/Entries.cs
@@ -16,19 +16,31 @@ namespace Blog {
         public Entries() {
             Console.WriteLine(nameof(Entries));
 
-            var postPaths = Directory.EnumerateFiles("posts", "*.md", SearchOption.AllDirectories)
-                .OrderByDescending(i => i)
-                .ToArray();
-
-            var articlePaths = Directory.GetFiles("articles", "*.md", SearchOption.AllDirectories);
+            InitPaths(out var postPaths, out var articlePaths);
 
             var pageCount = postPaths.Length / NPP;
             if(pageCount * NPP < postPaths.Length)
                 pageCount++;
+            if(pageCount == 0)
+                pageCount = 1;
 
             _postPaths = postPaths;
             _articlePaths = articlePaths;
             PageCount = pageCount;
+        }
+
+        static void InitPaths(out string[] posts, out string[] articles) {
+            posts = articles = Array.Empty<string>();
+
+            if(Directory.Exists("posts")) {
+                posts = Directory.EnumerateFiles("posts", "*.md", SearchOption.AllDirectories)
+                    .OrderByDescending(i => i)
+                    .ToArray();
+            }
+
+            if(Directory.Exists("articles")) {
+                articles = Directory.GetFiles("articles", "*.md", SearchOption.AllDirectories);
+            }
         }
 
         public IEnumerable<string> PostPaths {
